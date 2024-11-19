0d0ee42df845880586a5b7068c96e0e54a56f442
Daniel Dramond <daniel.dramond@theverygroup.com>
2024-11-19 13:29:53 +0000

    Initial Commit

---
+++ FacebookUI/FacebookUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookUIApp.swift
+//  FacebookUI
+//
+//  Created by Daniel Dramond on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FacebookUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
